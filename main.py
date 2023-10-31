ec30c5c77fef731742d76b3c5f5847fc946f13a9
Ricky <c0a221519e@edu.teu.ac.jp>
2023-10-31 17:48:49 +0900

    追加機能score_ver2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-from score import increase_score
 img_bg = pygame.image.load("ex05/images/bg.jpg")
 
 bg_y = 0
@@ -17,13 +16,15 @@ def main():
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
 
+    tmr = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         #スコアを増加
-        score, _ = increase_score(score,0,1000)
+        if score < 9999999 and tmr % 400 == 0:
+            score += 1
 
         bg_y = (bg_y + 1) % 600
         screen.blit(img_bg,[0,bg_y - 600])
@@ -35,6 +36,7 @@ def main():
         screen.blit(text_score,(10,10))
 
         pygame.display.update()
+        tmr += 1
         clock.tick(120)
         
 if __name__ == "__main__":
